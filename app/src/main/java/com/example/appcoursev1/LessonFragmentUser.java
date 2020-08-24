c3fff2fbe33b85595ba98036a2fa19c645072a6c
ltd-coding <57103909+ltd-coding@users.noreply.github.com>
2020-08-24 14:29:31 +0700

    Commit 4: Chức năng Test: hiển thị câu hỏi, câu trả lời theo db, đổi màu khi btn được click, cộng điểm Chức năng Lesson:hiển thị bài học, phân chia theo id trong sql, 3 nút previous,next,exit để điều hướng P/S: Xóa hẳn app cũ và data cache của app cũ rồi mới tải, do có thay đổi trong database

--- app/src/main/java/com/example/appcoursev1/LessonFragmentUser.java
+++ app/src/main/java/com/example/appcoursev1/LessonFragmentUser.java
@@ -1,23 +1,70 @@
 package com.example.appcoursev1;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class LessonFragmentUser extends Fragment {
+public class LessonFragmentUser extends Fragment implements View.OnClickListener{
+    private Button btnLesson1,btnLesson2;
+    private Button btn[]=new Button[4];
+    private Context context;
+    private View view;
+    private Intent intent;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //Code for this fragment
-        Context context=getActivity();
-        Toast.makeText(context, "User do something", Toast.LENGTH_SHORT).show();
-        return inflater.inflate(R.layout.fragment_lesson_user,container,false);
+        context=getActivity();
+        view=inflater.inflate(R.layout.fragment_lesson_user,container,false);
+        btn[0]=(Button) view.findViewById(R.id.lesson1);
+        btn[1]=(Button) view.findViewById(R.id.lesson2);
+        btn[2]=(Button) view.findViewById(R.id.lesson3);
+        btn[3]=(Button) view.findViewById(R.id.lesson4);
+        //Toast.makeText(context, "User do something", Toast.LENGTH_SHORT).show();
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        for (int i=0;i<4;i++) {
+            btn[i].setOnClickListener(this);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.lesson1:
+                intent=new Intent(context,LessonIntent.class);
+                intent.putExtra(MainActivity.EXTRA_NUMBER,0);
+                startActivity(intent);
+                break;
+            case R.id.lesson2:
+                intent=new Intent(context,LessonIntent.class);
+                intent.putExtra(MainActivity.EXTRA_NUMBER,3);
+                startActivity(intent);
+                break;
+            case R.id.lesson3:
+                intent=new Intent(context,LessonIntent.class);
+                intent.putExtra(MainActivity.EXTRA_NUMBER,6);
+                startActivity(intent);
+                break;
+            case R.id.lesson4:
+                intent=new Intent(context,LessonIntent.class);
+                intent.putExtra(MainActivity.EXTRA_NUMBER,9);
+                startActivity(intent);
+                break;
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/appcoursev1/LessonIntent.java
@@ -0,0 +1,103 @@
+package com.example.appcoursev1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class LessonIntent extends AppCompatActivity implements View.OnClickListener {
+    private DataBase dbLogin;
+    private SQLiteDatabase db;
+    private int data,numLesson; //data = id of lesson, numLesson= number of id used to save the lesson
+    private Cursor c;
+    private TextView name,pos,neg,ques;
+    private Button btnPre,btnNext;
+    private ImageButton btnExit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lesson_intent);
+
+        //setup
+        name=(TextView) findViewById(R.id.lessonName);
+        pos=(TextView) findViewById(R.id.lessonPos);
+        neg=(TextView) findViewById(R.id.lessonNeg);
+        ques=(TextView) findViewById(R.id.lessonQues);
+
+        btnPre=(Button)findViewById(R.id.previousL);
+        btnNext=(Button) findViewById(R.id.nextL);
+        btnExit=(ImageButton)findViewById(R.id.lesson_exit);
+
+
+        //---
+        //data from sql
+        dbLogin = new DataBase(this);
+        try {
+            dbLogin.updateDataBase();
+        } catch (IOException mIOException) {
+            throw new Error("UnableToUpdateDatabase");
+        }
+        try {
+            db = dbLogin.getWritableDatabase();
+        } catch (SQLException mSQLException) {
+            throw mSQLException;
+        }
+        numLesson=3;
+        data=getIntent().getIntExtra(MainActivity.EXTRA_NUMBER,0);
+        c=dbLogin.GetData("SELECT * FROM LESSON LIMIT '"+ data +"','"+ numLesson +"'");
+        c.moveToFirst();
+
+        btnNext.setOnClickListener(this);
+        btnPre.setOnClickListener(this);
+        btnExit.setOnClickListener(this);
+        reloadData();
+    }
+
+    private void reloadData() {
+        name.setText(c.getString(1));
+        pos.setText(c.getString(2));
+        neg.setText(c.getString(3));
+        ques.setText(c.getString(4));
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.previousL:
+                if (c.isFirst()) {
+                    c.moveToLast();
+                }
+                else {
+                    c.moveToPrevious();
+                }
+                reloadData();
+                break;
+            case R.id.nextL:
+                if (c.isLast()) {
+                    c.moveToFirst();
+                }
+                else {
+                    c.moveToNext();
+                }
+                reloadData();
+                break;
+            case R.id.lesson_exit:
+                finish();
+                break;
+        }
+
+    }
+}
